67d1c3dee1f08153a275fbe0dddb96d6d908519b
Vianney de Boisredon <v2boisredon@hotmail.fr>
2018-11-23 17:12:12 +0100

    front end is decent, now herokuuuuu

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,10 @@
 require 'json'
 require 'open-uri'
 
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
+
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 user_serialized = open(url).read
 ingredients = JSON.parse(user_serialized)
